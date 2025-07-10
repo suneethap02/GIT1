a22be6997fa88086852282dc405416bc6a313c17
suneetha <suneetha.p02@gmail.com>
2025-07-10 11:46:13 +0530

    first message

---
+++ CoreJava/src/PACK1/PS1.java
@@ -0,0 +1,12 @@
+package PACK1;
+
+public class PS1 {
+
+	public static String add()
+	{
+		// TODO Auto-generated method stub
+System.out.println("this is from PS1");
+return "aa";
+	}
+
+}
---
+++ CoreJava/src/PACK1/PS2.java
@@ -0,0 +1,11 @@
+package PACK1;
+//InheritanCe---ACquiring one Class of methods and variables into another Class 
+public class PS2 extends PS1{
+
+	public static void PS2()
+	{
+		// TODO Auto-generated method stub
+		add();
+		}
+
+}
